ee400558af3684a67ef05bbe32279523f3218f8c
María Isabel Álvarez Hernández <maria.alvarez9409@gmail.com>
2023-02-15 15:13:51 -0600

    add routes

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -3,13 +3,26 @@ import { StyleSheet, View } from "react-native";
 import theme from "../theme";
 import StyledText from "./StyledText";
 import Constants from "expo-constants";
+import { Link } from "react-router-native";
 
+const AppBarTab = ({ active, children, to }) => {
+  return (
+    <Link to={to}>
+      <StyledText fontWeight="bold" style={styles.text}>
+        {children}
+      </StyledText>
+    </Link>
+  );
+};
 const AppBar = () => {
   return (
     <View style={styles.container}>
-      <StyledText fontWeight="bold" style={styles.text}>
+      <AppBarTab active to="/">
         Repositories
-      </StyledText>
+      </AppBarTab>
+      <AppBarTab active to="/signin">
+        Sign In
+      </AppBarTab>
     </View>
   );
 };
@@ -20,6 +33,9 @@ const styles = StyleSheet.create({
     paddingTop: Constants.statusBarHeight + 10,
     paddingBottom: 10,
     paddingLeft: 10,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingHorizontal: 10,
   },
   text: {
     color: theme.appBar.textPrimary,
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,12 +2,21 @@ import React from "react";
 import { View } from "react-native";
 import RepositoryList from "./RepositoryList";
 import AppBar from "./AppBar";
+import { Routes, Route, Redirect } from "react-router-native";
+import { Text } from "react-native";
 
 const Main = () => {
   return (
     <View style={{ flex: 1 }}>
       <AppBar />
-      <RepositoryList />
+      <Routes>
+        <Route path="/" exact element={<RepositoryList />} />
+        <Route
+          path="/signin"
+          exact
+          element={<Text>Sign In - workin on in</Text>}
+        />
+      </Routes>
     </View>
   );
 };
